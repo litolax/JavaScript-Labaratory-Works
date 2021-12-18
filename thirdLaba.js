439dbbe9b4dcdedafd24fe9155708fdfafc215c4
litolax <ura_rat@mail.ru>
2021-12-18 11:56:39 +0300

    Продолжил делать третью лабу

--- thirdLaba.js
+++ thirdLaba.js
@@ -78,6 +78,49 @@ function third(){
     }
 }
 
+function fourth(){
+    let a = prompt("Введите a");
+    if (a > 0 && a < 5) alert("Верно");
+    else alert("Неверно");
+
+    if (+a === 0 || +a === 2) alert("a = " + (a + 7));
+    else alert("a = " +(a / 10));
+
+}
+
+function fifth(){
+    let a = prompt("Введите a");
+    let b = prompt("Введите b");
+    if (a <= 1 && b >= 3) alert(a + b);
+    else alert(a - b);
+}
+
+function sixth(){
+    let num = prompt("Введите num");
+    let result = undefined;
+    switch(num){
+        case 1: {
+            result = 'зима';
+            break;
+        }
+        case 2: {
+            result = 'весна';
+            break;
+        }
+        case 3: {
+            result = 'лето';
+            break;
+        }
+        case 4: {
+            result = 'осень';
+            break;
+        }
+    }
+    alert(result);
+}
+
+function 
+
 // first();
 // second();
 // third();
